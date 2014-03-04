529edcb11ae326bde9d6a95d04b6725ce5eaef37
v.zapolskydovnar@gmail.com <v.zapolskydovnar@gmail.com>
2014-03-04 16:47:56 +0200

    files are generated dynamically now: 1. SCSS -> CSS 2. templates -> HTML 3. images -> images artists section update in progress

---
+++ Gruntfile.js
@@ -0,0 +1,92 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '',
+        watch: {
+            styles: {
+                files: './dev/sass/**.*',
+                tasks: ['sass:dev'],
+                options: {
+                    interrupt: true
+                }
+            },
+            templates: {
+                files: './dev/templates/**.*',
+                tasks: ['includes:build'],
+                options: {
+                    interrupt: true
+                }
+            }
+        },
+        sass: {
+            dev: {
+                options: {
+                    trace: true,
+                    noCache: true,
+                    style: 'nested'
+                },
+                files: [
+                    {
+                        expand: true,
+                        debugInfo: true,
+                        cwd: './dev/sass',
+                        src: ['*.scss'],
+                        dest: './web/css',
+                        ext: '.css'
+                    }
+                ]
+            },
+            prod: {
+                options: {
+                    style: 'compressed',
+                    noCache: true
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: './dev/sass',
+                        src: ['*.scss'],
+                        dest: './web/css',
+                        ext: '.css'
+                    }
+                ]
+            }
+        },
+        includes: {
+            build: {
+                cwd: './dev/templates',
+                src: [ '*.html'],
+                dest: 'web/',
+                options: {
+                    flatten: true,
+                    includePath: './dev/templates/include'
+                }
+            }
+        },
+        copy: {
+            images: {
+                expand: true,
+                cwd: './dev/templates/images/',
+                src: '**/*',
+                dest: './web/images/',
+                mode: true
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-includes');
+
+    // Default task.
+    grunt.registerTask('default', ['sass:dev']);
+    grunt.registerTask('watchall', ['watch:styles']);
+    grunt.registerTask('prod', ['sass:prod', 'includes:build', 'copy:images']);
+    grunt.registerTask('dev', ['sass:dev', 'includes:build', 'copy:images']);
+
+};
